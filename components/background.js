b830f337e8a07f79e70ab3e390420ac14413fcdd
Hunjoo Lee <hjbv.lee@gmail.com>
2025-01-12 20:28:32 -0500

    Update background.js

--- components/background.js
+++ components/background.js
@@ -2,4 +2,4 @@ const images = ["blackdesk.jpg", "pinkdesk.jpg", "mintdesk.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url('../image/${chosenImage}')`;
+document.body.style.backgroundImage = `url('/image/${chosenImage}')`;
